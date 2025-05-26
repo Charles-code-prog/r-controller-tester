4e0591dbce61e49482ea522ca344caf36034906a
Romão Charles <romaocharles.rpe@gmail.com>
2025-05-26 13:57:18 -0400

    Leitura de IR corrigida com SPI

---
+++ SPI_MASTER_IR/SPI_MASTER_IR.ino
@@ -0,0 +1,64 @@
+#include <SPI.h>
+#include <IRremote.hpp>
+
+#define IR_PIN 7
+#define SS1 2
+#define SS2 3
+#define SS3 4
+
+char resposta[3];
+bool IR_recebido = false;
+void setup() {
+  Serial.begin(115200);
+  SPI.begin();
+  
+  IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK);
+
+
+  pinMode(SS1, OUTPUT);
+  pinMode(SS2, OUTPUT);
+  pinMode(SS3, OUTPUT);
+
+  digitalWrite(SS1, HIGH);
+  digitalWrite(SS2, HIGH);
+  digitalWrite(SS3, HIGH);
+
+  delay(1000);
+}
+void loop() {
+  if (IrReceiver.decode()) {
+    IR_recebido = true;
+    IrReceiver.resume();
+  }
+  
+  if (Serial.available()) {
+    char c = Serial.read();
+    if (c == '1') {
+      String linha = "";
+
+      linha = IR_recebido ? "OK;" : "0;";
+      IR_recebido = false;
+
+      linha += requestFromSlave(SS1);
+      linha += requestFromSlave(SS2);
+      linha += requestFromSlave(SS3);
+      linha += "II;";
+      Serial.println(linha);
+    }
+  }
+
+}
+String requestFromSlave(int ssPin) {
+  digitalWrite(ssPin, LOW);
+  delayMicroseconds(10);  // tempo mínimo para ativar SS
+
+  resposta[0] = SPI.transfer('1');    // envia comando, recebe 1º byte
+  delayMicroseconds(50);              // permite o escravo preparar o próximo byte
+  resposta[1] = SPI.transfer(0x00);   // recebe 2º byte
+
+  digitalWrite(ssPin, HIGH);
+
+  // Verifica se qualquer um dos dois bytes é '5'
+  if (resposta[0] == '5' || resposta[1] == '5') return "OK;";
+  else return "0;";
+}
---
+++ SPI_SLAVE_IR/SPI_SLAVE_IR.ino
@@ -0,0 +1,38 @@
+#include <SPI.h>
+#include <IRremote.hpp>
+
+#define IR_PIN 7
+
+volatile bool IR_recebido = false;
+
+void setup() {
+  Serial.begin(115200);
+  IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK);
+
+  pinMode(MISO, OUTPUT);
+  pinMode(10, INPUT);           // SS deve ser entrada para modo escravo
+  SPCR |= _BV(SPE);             // Habilita SPI escravo
+  SPI.attachInterrupt();        // Habilita interrupção SPI
+}
+
+ISR(SPI_STC_vect) {
+  char comando = SPDR;
+
+  if (comando == '1') {
+    if (IR_recebido) {
+      SPDR = '5';        // Envia '5' se sinal IR foi detectado
+      IR_recebido = false; // Limpa o sinalizador (só responde uma vez)
+    } else {
+      SPDR = '0';        // Sem sinal IR, responde '0'
+    }
+  } else {
+    SPDR = ' ';          // Qualquer outro comando → responde espaço
+  }
+}
+
+void loop() {
+  if (IrReceiver.decode()) {
+    IR_recebido = true;            // Marca que recebeu sinal IR
+    IrReceiver.resume();           // Pronto para o próximo
+  }
+}
